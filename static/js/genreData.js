db56f717b3072c8c9ec784367c13e69723d004a2
rigsbyk <kathryn.rigsby@gmail.com>
2020-10-16 23:18:20 -0400

    updating index.html file and added legend to genre ratings over time chart, along with screenshots for presentation

--- static/js/genreData.js
+++ static/js/genreData.js
@@ -53,7 +53,7 @@ d3.json("/datajson.json", function(data) {
     .domain(mygroups)
     .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
 
-  
+   
   
   // Show the areas
   svg
@@ -69,6 +69,5 @@ d3.json("/datajson.json", function(data) {
         
     )
 
-  
-    
+ 
 });
